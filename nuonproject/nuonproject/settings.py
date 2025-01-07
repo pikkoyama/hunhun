ae7c76c5c0d29d5822e9c28500038f2ec15b63fc
pikkoyama <oom2325225@stu.o-hara.ac.jp>
2025-01-07 18:14:32 +0900

    APT

--- nuonproject/nuonproject/settings.py
+++ nuonproject/nuonproject/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # 追加アプリ
-    'guide.apps.GuideConfig'
+    'guide.apps.GuideConfig',
+    'manager.apps.ManagerConfig',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
