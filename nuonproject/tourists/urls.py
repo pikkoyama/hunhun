7fe87f8979d4a3df728510ab5a27dfae9b2d09df
UvuvwevwevweOnyetenyevweUgwemubwemAzuma <oom2325066@stu.o-hara.ac.jp>
2025-01-08 03:44:20 +0900

    残業

---
+++ nuonproject/tourists/urls.py
@@ -0,0 +1,29 @@
+# 小山 1/7-------------------------------------------
+from django.urls import path, include
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path(
+        "InformationPinChange/",
+        views.InformationPinChangeView.as_view(),
+        name="InformationPinChange"
+    ),
+    path(
+        "InformationPinRegistration/",
+        views.InformationPinRegistrationView.as_view(),
+        name="InformationPinRegistration"
+    ),
+    path(
+        "PasswordChange/",
+        views.PasswordChangeView.as_view(),
+        name="PasswordChange"
+    ),
+    path(
+        "TourRegistration/",
+        views.TourRegistrationView.as_view(),
+        name="TourRegistration"
+    )
+]
+# -----------------------------------------------------/
--- nuonproject/tourists/views.py
+++ nuonproject/tourists/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+# 我妻1/8--------------------------------
+from django.views.generic.base import TemplateView
+
+class InformationPinChangeView(TemplateView):
+    # あづーま
+    template_name = "InformationPinChange.html"
+
+class InformationPinRegistrationView(TemplateView):
+    # あづーま
+    template_name = "InformationPinRegistration.html"
+
+class PasswordChangeView(TemplateView):
+    # あづーま
+    template_name = "PasswordChange.html"
+
+class TourRegistrationView(TemplateView):
+    # あづーま
+    template_name = "TourRegistration.html"
+# ------------------------------------------/
