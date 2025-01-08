aed24c73886caf6e503ad1ab917269a5b6bac1d8
ryopee <gundam0078_rx@icloud.com>
2025-01-08 12:09:50 +0900

    accounts-models変更

--- nuonproject/accounts/models.py
+++ nuonproject/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+# 1/8 岩本 --------------------------------------------
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    
+    pass
