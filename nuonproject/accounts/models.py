5c57d9e76af9243ff7abff734c955367128efd6d
ryopee <gundam0078_rx@icloud.com>
2025-01-09 16:32:06 +0900

    wawawa

--- nuonproject/accounts/models.py
+++ nuonproject/accounts/models.py
@@ -1,6 +1,6 @@
-from django.db import models
+# from django.db import models
 
-# 1/8 岩本 --------------------------------------------
+# # 1/8 岩本 --------------------------------------------
 # from django.contrib.auth.models import AbstractUser
 # # Create your models here.
 
@@ -10,4 +10,8 @@ from django.db import models
 #     email = models.EmailField(verbose_name="メールアドレス",)
 #     password = models.CharField(verbose_name="パスワード", max_length=256)
 #     admin = models.BooleanField(verbose_name="管理者",default= False)
-#     pass
+
+#     class Meta:
+#         verbose_name_plural = 'カスタムユーザー'
+#     def __str__(self):
+#         return f"{self.name}({self.number})"
