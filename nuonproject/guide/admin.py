8959033aa6b5156eb1cde8f0dc5b5c9835367f44
negishima-saki <oom2325025@stu.o-hara.ac.jp>
2025-01-15 09:51:28 +0900

    あぱつ

--- nuonproject/guide/admin.py
+++ nuonproject/guide/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 # import * とすることでmodels.pyで定義した全てのモデルをインポートする
 from .models import *
+from django.contrib.auth.admin import UserAdmin
 
+# class CustomUserAdmin(UserAdmin):
+#     model = CustomUser
 # 作成したモデルを管理者画面で確認できるように登録する
+# admin.site.register(CustomUser,CustomUserAdmin)
 admin.site.register(CustomUser)
 admin.site.register(GuidePin)
 admin.site.register(Sort)
