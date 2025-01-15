b0746b8318347406597303608ef974ae49333435
UvuvwevwevweOnyetenyevweUgwemubwemAzuma <oom2325066@stu.o-hara.ac.jp>
2025-01-15 11:05:49 +0900

    アカウント作成に完全勝利した小魚系美少女

--- nuonproject/manager/views.py
+++ nuonproject/manager/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from guide.models import CustomUser
+from .forms import AccountCreationForm
 
 # 清原 1/8--------------------------------
 from django.views.generic.base import TemplateView
@@ -11,9 +15,22 @@ class AdminViewView(TemplateView):
     # さくちゃんTV
     template_name = "AdminViewAccount.html"
 
-class AdminCreateView(TemplateView):
-    # さくちゃんTV
-    template_name = "AdminCreateAccount.html"
+# 1/15 あづまーと-------------------
+class AdminCreateView(CreateView):
+    model = CustomUser
+    form_class = AccountCreationForm
+    template_name = "AdminCreateAccount.html"  # 使用するテンプレート名
+    success_url = reverse_lazy('manager:AdminTop')  # アカウント作成後にリダイレクトするURL
+    
+    # フォームが保存された後、パスワードをハッシュ化して保存する
+    def form_valid(self, form):
+        # 通常の保存処理を行う前にパスワードをハッシュ化
+        password = form.cleaned_data.get('password')
+        account = form.save(commit=False)  # 保存しない状態でインスタンスを取得
+        account.set_password(password)  # パスワードをハッシュ化
+        account.save()  # 保存
+        return super().form_valid(form)
+# --------------------------------
 
 class AdminSuccessView(TemplateView):
     # さくちゃんTV
