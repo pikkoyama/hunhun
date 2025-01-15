c5a80823f82bc75f4834c295cc33c01ad9e283a5
pikkoyama <oom2325225@stu.o-hara.ac.jp>
2025-01-15 10:40:05 +0900

    forms

--- nuonproject/guide/forms.py
+++ nuonproject/guide/forms.py
@@ -11,11 +11,12 @@ from django.forms.widgets import CheckboxSelectMultiple, EmailInput
 # バリデーション用のライブラリをインポート
 from django.forms import ValidationError
 from django.views.generic.edit import CreateView
+from django import forms
 
 #########################################################
 # 1/14 小山
 ###事例登録のフォームを定義
-class CaseRegistrationForm(CreateView):
+class CaseRegistrationForm(forms.form):
     '''CaseRegistrationForm のサブクラス
     '''
     class Meta:
@@ -28,7 +29,7 @@ class CaseRegistrationForm(CreateView):
         model = Case
         # フォームで使用するフィールドを設定（画面設計を参照）
         # 社員番号 タイトル カテゴリー 本文 
-        fields = ("number", "title", "category", "post_date")
+        fields = ("number", "title", "category", "main")
         
                 # エラーメッセージを指定
         error_messages = {
