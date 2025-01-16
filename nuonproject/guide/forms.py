389e6321262aae5d50f211686a38fc7149942d68
pikkoyama <oom2325225@stu.o-hara.ac.jp>
2025-01-16 11:34:02 +0900

    ああ

--- nuonproject/guide/forms.py
+++ nuonproject/guide/forms.py
@@ -27,19 +27,20 @@ class CaseRegistrationForm(forms.ModelForm):
 
     # 必要に応じてフィールドのウィジェットやラベルを設定できます
    case_number = forms.CharField(max_length=30, label='事例番号')
-   #  
+   #  デフォルトにしたい@小山
    number = forms.CharField(max_length=8,widget=forms.TextInput(attrs={'type':'text'}),label="社員番号" )
    title = forms.CharField(max_length=30, label='タイトル')
    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='カテゴリ', empty_label="カテゴリを選択")
    main = forms.CharField(widget=forms.Textarea, label='本文')
    post_date = forms.DateField(initial=timezone.now, widget=forms.DateInput(attrs={'type': 'date'}), label="投稿日")
 
-   def __init__(self, *args, **kwargs):
-        # 現在のログインユーザーを取得
-        user = kwargs.get('user')  # ビューから渡されるユーザー情報
+# デフォルト
+#    def __init__(self, *args, **kwargs):
+#         # 現在のログインユーザーを取得
+#         user = kwargs.get('user')  # ビューから渡されるユーザー情報
 
-        super().__init__(*args, **kwargs)
+#         super().__init__(*args, **kwargs)
 
-        # ユーザーが存在する場合、社員番号を自動で入力
-        if user:
-            self.fields['number'].initial = user.profile.employee_number  # ユーザーの社員番号を設定
+#         # ユーザーが存在する場合、社員番号を自動で入力
+#         if user:
+#             self.fields['number'].initial = user.profile.employee_number  # ユーザーの社員番号を設定
