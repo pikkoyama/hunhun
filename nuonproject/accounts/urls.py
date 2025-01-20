ccecab997621db1f3b7c472dfd0706961d975ac4
UvuvwevwevweOnyetenyevweUgwemubwemAzuma <oom2325066@stu.o-hara.ac.jp>
2025-01-20 12:24:46 +0900

    メール送信

--- nuonproject/accounts/urls.py
+++ nuonproject/accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 from manager.views import admin_dashboardView
 from guide.views import homeView
-from .views import CustomLogoutView
+from .views import CustomLogoutView,PasswordResetDoneView,PasswordResetConfirmView
 
 app_name = 'accounts'
 
@@ -23,8 +23,12 @@ urlpatterns = [
         views.PasswordEmailView.as_view(),
         name="PasswordEmail"
     ),
+    path('password_reset_done/',PasswordResetDoneView.as_view(),name='password_reset_done'),
     path('admin_dashboard/', admin_dashboardView.as_view(), name='admin_dashboard'),  # 管理者用ダッシュボードのURL
     path('home/', homeView.as_view(), name='home'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('change_password/', views.ChangePasswordView.as_view(), name='change_password'),
+    path('password_reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    
 ]
 # -----------------------------------------------------/
