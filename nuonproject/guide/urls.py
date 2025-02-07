d0fb2cdb7f005e873adf9bdcd0871fc74fe9728d
ryopee <gundam0078_rx@icloud.com>
2025-02-07 12:48:51 +0900

    urls

--- nuonproject/guide/urls.py
+++ nuonproject/guide/urls.py
@@ -1,4 +1,3 @@
-# 小山 11/28-------------------------------------------
 from django.urls import path
 from . import views
 from django.urls import path
@@ -7,41 +6,110 @@ from .views import DeletePinView
 
 app_name = 'guide'
 
-# -----------------------------------------------------/
 # 1/8小山-----------------------------------------------
+# 2/7岩本-----------------------------------------------
 urlpatterns = [
-    path('caselist/', views.CaseListView.as_view(), name='caselist'),
-    path('caselist/<int:case_id>/', CaseListView.comment_view, name='comment_view'),
-    path('delete/', views.DeleteCaseView.as_view(), name='delete'),
-    path('caseregistration/', views.CaseRegistrationView.as_view(), name='caseregistration'),
-    # path('selectpref/', views.SelectPrefView.as_view(), name='selectpref'),
-    path('guidancepindelete/', views.GuidancePinDeleteView.as_view(), name='guidancepindelete'),
-    path('guidetop/', views.GuideTopView.as_view(), name='guidetop'),
-    # koyama --------------------
-    path('guidemap/', views.GuideMapView.as_view(), name='guidemap'),
-    path('guidemap/<str:tour_number>/', views.GuideMapView.as_view(), name='guidemap'),
-    
-    # ---------------------------
-    path('toursearch/', views.TourSearchView.as_view(), name='toursearch'),
-    path('tour/<str:tour_number>/edit/', views.TourChangeView.as_view(), name='tourchange'),
+    # トップページ画面URL
+    path('guidetop/',
+         views.GuideTopView.as_view(),
+         name='guidetop'),
+    # パスワード変更URL
+    path("passwordchange/",
+         views.PasswordChangeView.as_view(),
+         name="passwordchange"),
 
-    path("Informationpinchange/",views.InformationPinChangeView.as_view(),name="informationpinchange"),
-    path("Informationpinregistration/",views.InformationPinRegistrationView.as_view(),name="informationpinregistration"),
-    path("tourregistration/",views.TourRegistrationView.as_view(),name="tourregistration"),
-    path("passwordchange/",views.PasswordChangeView.as_view(),name="passwordchange"),
-    # path("caseregistconfirmation/",views.CaseRegistConfirmationView.as_view(),name="caseregistconfirmation"),
-    # 根岸
-    path('authorize_case/', AuthorizeCaseView.as_view(), name='authorize_case'),
-    path('tour/<str:tour_number>/qr/', QRCodeView.as_view(), name='qr_code_view'),
-    path('case/<str:case_number>/edit/', views.CaseChangeView.as_view(), name='case_edit'),
-    path('delete_comment/', views.DeleteCommentView.as_view(), name='delete_comment'),
-    path('toursearch/namesearch/',views.TourNameListView.as_view(), name='toursearch/namesearch'),
-    path('toursearch/numbersearch/',views.TourNumberListView.as_view(), name='toursearch/numbersearch'),
-    path('caselist/titlesearch/',views.CaseTitleListView.as_view(), name='caselist/titlesearch'),
-    path('caselist/sortesearch/',views.CaseSortListView.as_view(), name='caselist/sortsearch'),
-    path('tour/<int:tour_id>/pin/<int:pk>/edit/', views.InformationPinUpdateView.as_view(), name='edit_information_pin'),
-    path('delete_pin/<int:pin_id>/', DeletePinView.as_view(), name='delete_pin'),
+    # ---------事例関連URL-----------
+    # 事例閲覧URL
+    path('caselist/',
+         views.CaseListView.as_view(),
+         name='caselist'),
+    # 事例コメントURL
+    path('caselist/<int:case_id>/',
+         CaseListView.comment_view,
+         name='comment_view'),
+    # 事例削除URL(管理者専用)
+    path('delete/',
+         views.DeleteCaseView.as_view(),
+         name='delete'),
+    # 事例登録URL
+    path('caseregistration/',
+         views.CaseRegistrationView.as_view(),
+         name='caseregistration'),
+    # 事例変更URL
+    path('case/<str:case_number>/edit/',
+         views.CaseChangeView.as_view(),
+         name='case_edit'),
+    # 事例タイトル検索URL
+    path('caselist/titlesearch/',
+         views.CaseTitleListView.as_view(),
+         name='caselist/titlesearch'),
+    # 事例ソート検索URL
+    path('caselist/sortesearch/',
+         views.CaseSortListView.as_view(),
+         name='caselist/sortsearch'),
+    # コメント削除URL(入力者専用)
+    path('delete_comment/',
+         views.DeleteCommentView.as_view(),
+         name='delete_comment'),
+    # 事例認可URL(管理者画面)
+    path('authorize_case/',
+         AuthorizeCaseView.as_view(),
+         name='authorize_case'),
 
+    # ---------ツアー関連URL-----------
+    # ツアー閲覧URL
+    path('toursearch/',
+         views.TourSearchView.as_view(),
+         name='toursearch'),
+    # ツアー変更URL
+    path('tour/<str:tour_number>/edit/',
+         views.TourChangeView.as_view(),
+         name='tourchange'),
+    # ツアータイトル検索URL
+    path('toursearch/namesearch/',
+         views.TourNameListView.as_view(),
+         name='toursearch/namesearch'),
+    # ツアー番号検索URL
+    path('toursearch/numbersearch/',
+         views.TourNumberListView.as_view(),
+         name='toursearch/numbersearch'),
+    # ツアーQRコード表示URL
+    path('tour/<str:tour_number>/qr/',
+         QRCodeView.as_view(),
+         name='qr_code_view'),
+    # ツアー登録画面URL
+    path("tourregistration/",
+         views.TourRegistrationView.as_view(),
+         name="tourregistration"),
+
+    # ---------ガイドマップ関連URL-----------
+    # ガイドマップURL
+    path('guidemap/',
+         views.GuideMapView.as_view(),
+         name='guidemap'),
+    # ガイドマップURL(ツアー別)
+    path('guidemap/<str:tour_number>/',
+         views.GuideMapView.as_view(),
+         name='guidemap'),
+    # ガイドピン削除URL
+    path('guidancepindelete/',
+         views.GuidancePinDeleteView.as_view(),
+         name='guidancepindelete'),
+    # ガイドピン変更URL
+    path("Informationpinchange/",
+         views.InformationPinChangeView.as_view(),
+         name="informationpinchange"),
+    # ガイドピン登録URL
+    path("Informationpinregistration/",
+         views.InformationPinRegistrationView.as_view(),
+         name="informationpinregistration"),
+    # # ピン変更URL
+    # path('tour/<int:tour_id>/pin/<int:pk>/edit/',
+    #      views.InformationPinUpdateView.as_view(),
+    #      name='edit_information_pin'),
+    # ガイドピン削除URL
+    path('delete_pin/<int:pin_id>/',
+         DeletePinView.as_view(),
+         name='delete_pin'),
 
 ]
-# ---------------------------------------------------------
